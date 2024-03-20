f63ff75caffe91bc31f5efda13bc986a9cf808ad
Tom Bautz <tom.bautz@axelspringer.de>
2024-03-20 16:29:24 +0100

    add static media files

--- Backend/app.js
+++ Backend/app.js
@@ -3,30 +3,33 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 app.use(cors());
+
+const backendURL = "http://localhost:3000";
 const videos = [
     {
-        thumbnailURL: "https://images.pexels.com/photos/20623990/pexels-photo-20623990/free-photo-of-blaumeise-vertraumt.jpeg",
+        thumbnailURL: backendURL + "/assets/blaumeise.jpeg",
         title: "die Blaumeise",
-        videoURL: "https://vod-progressive.akamaized.net/exp=1710837945~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F4467%2F14%2F372335193%2F1547101020.mp4~hmac=81e082153759b3581e27ed0d08cd87913a5a31069ace7b38ba01048b0e90f21e/vimeo-prod-skyfire-std-us/01/4467/14/372335193/1547101020.mp4?download=1&filename=video+%282160p%29.mp4",
+        videoURL: backendURL + "/assets/1.mp4",
         id: uuidv4(),
     },
 
     {
-        thumbnailURL: "https://images.pexels.com/photos/34299/herbs-flavoring-seasoning-cooking.jpg",
+        thumbnailURL: backendURL + "/assets/herbs.jpg",
         title: "herbs and spices",
-        videoURL: "https://vod-progressive.akamaized.net/exp=1710801777~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F4363%2F14%2F371817283%2F1544168334.mp4~hmac=c58dd68291696e7848bc36defbdecbbdb5f6ad9933fe18d460e060530afd6ab1/vimeo-prod-skyfire-std-us/01/4363/14/371817283/1544168334.mp4?download=1&filename=video+%282160p%29.mp4",
+        videoURL: backendURL + "/assets/2.mp4",
         id: uuidv4(),
     },
     {
-        thumbnailURL: "https://images.pexels.com/photos/4033148/pexels-photo-4033148.jpeg",
+        thumbnailURL: backendURL + "/assets/random.jpeg",
         title: "some random scientist",
-        videoURL: "https://vod-progressive.akamaized.net/exp=1710781187~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F1217%2F16%2F406087408%2F1739359232.mp4~hmac=0039c66ba7b94d00802cbdec3aad82aca329ef8ca434ad6ed22ca1dfe5536591/vimeo-prod-skyfire-std-us/01/1217/16/406087408/1739359232.mp4?download=1&filename=production_id%3A4114797+%282160p%29.mp4",
+        videoURL: backendURL + "/assets/3.mp4",
         id: uuidv4(),
     }
 
 
 ];
 
+app.use('/assets/', express.static('assets'))
 
 app.get('/videos', function (req, res) {
     const query = req.query.q;
