7676819bddf4b202230e548717cee49ad85c9a2e
Mika Alber <mika.alber@dynamicdudes.com>
2024-03-21 10:59:37 +0100

    added env variable

--- Backend/app.js
+++ Backend/app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-const backendURL = "http://localhost:3000";
+const backendURL = process.env.BACKEND_URL || "http://localhost:3000";
 const videos = [
     {
         thumbnailURL: backendURL + "/assets/blaumeise.jpeg",
